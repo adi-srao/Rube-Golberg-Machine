6fd4519e4517ac80f761b30e290dd97abd1af668
Aditya Rao <adityaraosunil@gmail.com>
2025-12-08 02:48:06 +0530

    fixed blinn-phong vertex shader

--- A3/Source/Shaders/Blinn_Phong/vertex_shader.glsl
+++ A3/Source/Shaders/Blinn_Phong/vertex_shader.glsl
@@ -1,5 +1,6 @@
-attribute vec3 a_position;
-attribute vec3 a_normal;
+// Three.js automatically provides these attributes
+// attribute vec3 position;  // provided by Three.js
+// attribute vec3 normal;    // provided by Three.js
 
 uniform mat4 u_model;
 uniform mat4 u_viewProjection;
@@ -8,11 +9,12 @@ varying vec3 v_normal;
 varying vec3 v_position;
 
 void main() {
-    vec4 worldPosition = u_model * vec4(a_position, 1.0);
+    // Use Three.js's built-in attributes: position and normal
+    vec4 worldPosition = u_model * vec4(position, 1.0);
     v_position = worldPosition.xyz;
     
     mat3 normalMatrix = mat3(u_model);
-    v_normal = normalize(normalMatrix * a_normal);
+    v_normal = normalize(normalMatrix * normal);
     
     gl_Position = u_viewProjection * worldPosition;
 }
